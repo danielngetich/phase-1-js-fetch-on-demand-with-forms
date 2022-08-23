6ca0e7c390aebda321b63e18eb15c3f20a891d7e
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-08-24 02:10:42 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-const init = () => {
-  
-}
+document.addEventListener('DOMContentLoaded', function(){
+let btn=document.querySelector("form")
+btn.addEventListener('submit',function(e){
+e.preventDefault();
+let n=(e.target.searchByID.value)
+fetch(`http://localhost:3000/movies/${n}`)
+.then(movie=>movie.json())
+.then(data=>{
+    const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
 
-document.addEventListener('DOMContentLoaded', init);
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+})
+  })
+})
